ee2ab4d5e106c6795e89b4fe8355b97289a82f2d
sliscers <info@localdesign.nl>
2015-10-05 12:24:20 +0200

    changes

---
+++ WindesHeim Game/WindesHeim Game/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindesHeim_Game
+{
+    public class Model
+    {
+        protected Controller controller;
+
+        public Model(Controller controller)
+        {
+            this.controller = controller;
+        }
+
+        public virtual void ControlsInit(Form GameWindow)
+        {
+        }
+
+        public virtual void GraphicsInit(Graphics g)
+        {
+        }
+    }
+    public class ModelMenu : Model
+    {
+
+        public ModelMenu(Controller controller) : base(controller)
+        {
+
+        }
+    }
+}
